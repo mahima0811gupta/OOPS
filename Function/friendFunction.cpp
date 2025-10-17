4927d9b3b33b48bf9fae4a7dfcd3acdfe7ca6075
mahima0811gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-10-17 11:29:28 +0530

    Update comments on friend functions in C++
    Clarified the explanation of friend functions in C++.

--- Function/friendFunction.cpp
+++ Function/friendFunction.cpp
@@ -3,7 +3,9 @@
 // Normally, the private section of a class is not accessible from outside.
 // But sometimes, we want an outside function to access private/protected members.
 // For this, we use the concept of friend function.
-// 2. Key Points
+In C++, a friend function is a special function that is not a member of a class but still has access to the class’s private and protected members.
+It is declared inside the class using the keyword friend.
+    // 2. Key Points
 // A friend function cannot be called using an object of the class.
 // It is called like a normal function.
 // A friend function can access the private/protected resources of a class, but it needs an object of that class.

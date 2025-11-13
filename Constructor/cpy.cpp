cd9a3b719a0c89965aaf90e55e5ab9204fd59a32
Mahima Gupta <143950581+mahima0811gupta@users.noreply.github.com>
2025-11-13 14:22:44 +0530

    Implement copy constructor in Student class
    Added a copy constructor to the Student class and demonstrated its usage in main.

--- Constructor/cpy.cpp
+++ Constructor/cpy.cpp
@@ -37,3 +37,41 @@ int main(){
     Hero raju(ram); //copy constructor
    raju.print();
 }
+
+
+
+
+
+eg #include <iostream>
+using namespace std;
+
+class Student {
+private:
+    int age;
+
+public:
+    // Normal constructor
+    Student(int a) {
+        age = a;
+    }
+
+    // Copy constructor
+    Student(const Student &s) {
+        age = s.age;   // copying value
+    }
+
+    void show() {
+        cout << "Age = " << age << endl;
+    }
+};
+
+int main() {
+    Student s1(20);   // normal object
+    Student s2 = s1;  // copy constructor is called
+
+    s1.show();
+    s2.show();
+}
+output
+Age = 20
+Age = 20
